d9c1d1f6d86c0512f7e1b64d92e084c9c73b89e2
Pamod Malith <dev.pamodmalith@gmail.com>
2026-07-25 15:47:29 +0530

    feat: add hyprpolkitagent service to autostart processes

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -7,7 +7,8 @@
 -- Autostart necessary processes (like notifications daemons, status bars, etc.)
 -- Or execute your favorite apps at launch like this:
 
-hl.on("hyprland.start", function ()
+hl.on("hyprland.start", function()
   hl.exec_cmd("waybar")
   hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
 end)
