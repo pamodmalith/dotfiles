2bfcfa12f5750079ccb3f4f32f94a81c33c42f7e
Pamod Malith <dev.pamodmalith@gmail.com>
2026-07-25 23:36:15 +0530

    feat: add SwayNC configuration files including styles, colors, and main config

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -10,5 +10,6 @@
 hl.on("hyprland.start", function()
   hl.exec_cmd("waybar")
   hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("swaync")
   hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
 end)
