e2c8617fc2813c69760d4ae31d3bfa0e5b2acff1
Pamod Malith <dev.pamodmalith@gmail.com>
2026-07-27 00:41:43 +0530

    feat: add new screenshot menu script and remove old ss-menu script

---
+++ rofi/.config/rofi/scripts/screenshot-menu.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Define the options
+OPTIONS="1. Region\n2. Full Screen\n3. Active Monitor\n4. Active Window"
+
+# Pipe them into rofi
+CHOICE=$(echo -e "$OPTIONS" | rofi -dmenu -i -p "Screenshot" -lines 4)
+
+# Execute the corresponding script based on the selection
+case "$CHOICE" in
+    "1. Region")
+        ~/.config/hypr/scripts/screenshot-region.sh
+        ;;
+    "2. Active Monitor")
+        sleep 0.3 && ~/.config/hypr/scripts/screenshot-monitor.sh
+        ;;
+    "3. Full Screen")
+        sleep 0.3 && ~/.config/hypr/scripts/screenshot-full.sh
+        ;;
+    "4. Active Window")
+        sleep 0.3 && ~/.config/hypr/scripts/screenshot-window.sh
+        ;;
+esac
