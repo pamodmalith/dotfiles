5b11761de9e582468114b81a0df3c71658f664ac
Pamod Malith <dev.pamodmalith@gmail.com>
2026-07-27 03:01:19 +0530

    feat: add hypridle command to autostart processes

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -10,6 +10,7 @@
 hl.on("hyprland.start", function()
   hl.exec_cmd("waybar")
   hl.exec_cmd("awww-daemon")
+  hl.exec_cmd("hypridle")
   hl.exec_cmd("swaync")
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
   hl.exec_cmd("wl-paste --type image --watch cliphist store")
