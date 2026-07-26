d856037b5668c612b473f2132cefd370e9a09da6
Pamod Malith <dev.pamodmalith@gmail.com>
2026-07-27 00:20:12 +0530

    feat: update autostart commands and fix clipboard script path in programs configuration

--- hypr/.config/hypr/modules/programs.lua
+++ hypr/.config/hypr/modules/programs.lua
@@ -8,7 +8,7 @@ return {
   browser             = "firefox",
   launcher            = "~/.config/rofi/scripts/launcher.sh",
   menu                = "rofi -show run",
-  clipboard           = "rofi -modi clipboard:~/.local/bin/cliphist-rofi-img -show clipboard -show-icons",
+  clipboard           = "rofi -modi clipboard:~/.config/hypr/scripts/cliphist-rofi-img -show clipboard -show-icons",
   notification_center = "swaync-client -t",
   reload_waybar       = "~/.config/hypr/scripts/waybar-reload.sh",
   shutdown_hypr       = "command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'",
