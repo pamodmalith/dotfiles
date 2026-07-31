2d24961c94a73fc8ff58186fea0ab5262d8ce751
Pamod Malith <dev.pamodmalith@gmail.com>
2026-07-31 19:19:10 +0530

    fix: update autostart configuration by removing waybar and swaync commands

--- hypr/.config/hypr/modules/autostart.lua
+++ hypr/.config/hypr/modules/autostart.lua
@@ -8,13 +8,14 @@
 -- Or execute your favorite apps at launch like this:
 
 hl.on("hyprland.start", function()
-  hl.exec_cmd("waybar")
   hl.exec_cmd("awww-daemon")
   hl.exec_cmd("hypridle")
-  hl.exec_cmd("swaync")
+  hl.exec_cmd("wayle panel start")
   hl.exec_cmd("wl-paste --type text --watch cliphist store")
   hl.exec_cmd("wl-paste --type image --watch cliphist store")
   hl.exec_cmd("wl-clip-persist --clipboard regular")
   hl.exec_cmd("~/.config/hypr/scripts/battery-alert.sh")
   hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
+  -- hl.exec_cmd("waybar")
+  -- hl.exec_cmd("swaync")
 end)
