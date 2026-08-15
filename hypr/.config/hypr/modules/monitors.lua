bad5723201924ec04a7ba4d126f1e500064ec78b
Pamod Malith <dev.pamodmalith@gmail.com>
2026-08-15 14:30:00 +0530

    feat(monitors): bind default workspace numbers to two monitors

--- hypr/.config/hypr/modules/monitors.lua
+++ hypr/.config/hypr/modules/monitors.lua
@@ -23,3 +23,17 @@ hl.monitor({
     position = "auto-right",
     scale = "1",
 })
+
+-- Bind workspaces 1-5 to the laptop screen
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", default = true })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "3", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "4", monitor = "eDP-1" })
+hl.workspace_rule({ workspace = "5", monitor = "eDP-1" })
+
+-- Bind workspaces 6-10 to the external HDMI monitor
+hl.workspace_rule({ workspace = "6", monitor = "HDMI-A-1", default = true })
+hl.workspace_rule({ workspace = "7", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "8", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "9", monitor = "HDMI-A-1" })
+hl.workspace_rule({ workspace = "10", monitor = "HDMI-A-1" })
