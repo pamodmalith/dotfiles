3c717019b4586db8f070b651bf08e53748d3e3ee
Pamod Malith <dev.pamodmalith@gmail.com>
2026-09-18 23:30:56 +0530

    feat(colors): add Cachy color definitions for window decorations

--- hypr/.config/hypr/modules/decorations.lua
+++ hypr/.config/hypr/modules/decorations.lua
@@ -1,81 +1,46 @@
------------------------
----- LOOK AND FEEL ----
------------------------
+-- Look and feel configuration
 
--- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in          = 5,
-        gaps_out         = 10,
-
-        border_size      = 2,
-
-        col              = {
-            active_border   = { colors = { "rgba(33ccffee)", "rgba(00ff99ee)" }, angle = 45 },
-            inactive_border = "rgba(595959aa)",
+        gaps_in = 3,
+        gaps_out = 8,
+        border_size = 2,
+        extend_border_grab_area = 10,
+        resize_on_border = true,
+        col = {
+            active_border = {
+                colors = { CACHYLGREEN, CACHYDGREEN },
+                angle = 45,
+            },
+            inactive_border = CACHYGRAY,
         },
-
-        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false,
-
-        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-        allow_tearing    = false,
-
-        layout           = "dwindle", -- Set default layout to dwindle, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Layouts/
     },
-
+    group = {
+        col = {
+            border_active = CACHYLBLUE,
+            border_inactive = CACHYGRAY,
+            border_locked_active = CACHYDBLUE,
+            border_locked_inactive = CACHYGRAY,
+        },
+        groupbar = {
+            col = {
+                active = CACHYLGREEN,
+                inactive = CACHYGRAY,
+                locked_active = CACHYDBLUE,
+                locked_inactive = CACHYGRAY,
+            },
+        },
+    },
     decoration = {
-        rounding         = 10,
-        rounding_power   = 2,
-
-        -- Change transparency of focused and unfocused windows
-        active_opacity   = 1.0,
+        dim_special = 0.3,
+        rounding = 10,
+        active_opacity = 0.95,
         inactive_opacity = 0.85,
-
-        shadow           = {
-            enabled      = true,
-            range        = 4,
-            render_power = 3,
-            color        = "0xee1a1a1a",
+        fullscreen_opacity = 1,
+        blur = {
+            size = 5,
+            passes = 4,
+            special = true,
         },
-
-        blur             = {
-            enabled  = true,
-            size     = 30,
-            passes   = 1,
-            vibrancy = 0.1696,
-        },
-    },
-
-    animations = {
-        enabled = true,
     },
 })
-
--- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
-hl.curve("easeOutQuint", { type = "bezier", points = { { 0.23, 1 }, { 0.32, 1 } } })
-hl.curve("easeInOutCubic", { type = "bezier", points = { { 0.65, 0.05 }, { 0.36, 1 } } })
-hl.curve("linear", { type = "bezier", points = { { 0, 0 }, { 1, 1 } } })
-hl.curve("almostLinear", { type = "bezier", points = { { 0.5, 0.5 }, { 0.75, 1 } } })
-hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
-
--- Default springs
-hl.curve("easy", { type = "spring", mass = 1, stiffness = 71.2633, dampening = 15.8273644 })
-
-hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
-hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows", enabled = true, speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, spring = "easy", style = "popin 87%" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
-hl.animation({ leaf = "fadeIn", enabled = true, speed = 1.73, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeOut", enabled = true, speed = 1.46, bezier = "almostLinear" })
-hl.animation({ leaf = "fade", enabled = true, speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers", enabled = true, speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "layersIn", enabled = true, speed = 4, bezier = "easeOutQuint", style = "fade" })
-hl.animation({ leaf = "layersOut", enabled = true, speed = 1.5, bezier = "linear", style = "fade" })
-hl.animation({ leaf = "fadeLayersIn", enabled = true, speed = 1.79, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeLayersOut", enabled = true, speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn", enabled = true, speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true, speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "zoomFactor", enabled = true, speed = 7, bezier = "quick" })
